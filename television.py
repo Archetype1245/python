4c0cf4ad26db4fbf44ae6fd7524554167bd19285
Archetype1245 <timpippin1245@gmail.com>
2024-08-13 13:33:00 -0500

    Re-adding the changes made to television.py and re-adding the test_television.py file. Should ensure the proper file versions are in the proper branches for assignment requirements.

--- television.py
+++ television.py
@@ -6,7 +6,7 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
@@ -14,10 +14,16 @@ class Television:
 
         self.__volume_muted_at = self.__volume
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Turns on/off the television by changing the value of the status variable
+        """
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Mutes/unmutes the television (when it's on) by changing the value of the muted variable
+        """
         if self.__status:
             if not self.__muted:
                 self.__volume_muted_at = self.__volume  # records the volume muted at if not already muted
@@ -29,15 +35,26 @@ class Television:
             else:
                 self.__volume = self.__volume_muted_at
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Increase the tv channel value (when the tv is on). Will wrap around if at the maximum channel value;
+        ex: max_channel + 1 = min_channel
+        """
         if self.__status:
             self.__channel = (self.__channel + 1) if self.__channel < Television.MAX_CHANNEL else Television.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Decrease the tv channel value (when the tv is on). Will wrap around if at the minimum channel value;
+        ex: min_channel - 1 = max_channel
+        """
         if self.__status:
             self.__channel = (self.__channel - 1) if self.__channel > Television.MIN_CHANNEL else Television.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Increases the tv volume (when the tv is on). Does nothing if the volume value is at the maximum value.
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -45,7 +62,10 @@ class Television:
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Decreases the tv volume (when the tv is on). Does nothing if the volume value is at the minimum value.
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -53,5 +73,11 @@ class Television:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Sends the values of the tv object in the following format:
+        Power = [status], Channel = [channel], Volume = [volume];
+        where the items in brackets will hold the current values for those variables.
+        :return: String representation of the tv object.
+        """
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
---
+++ test_television.py
@@ -0,0 +1,105 @@
+import unittest
+import television
+
+
+class TestTelevision(unittest.TestCase):
+
+    def setUp(self):
+        self.tv = television.Television()
+
+    def test___init__(self):
+        self.assertFalse(self.tv._Television__status)
+        self.assertFalse(self.tv._Television__muted)
+        self.assertEqual(self.tv._Television__volume, television.Television.MIN_VOLUME)
+        self.assertEqual(self.tv._Television__channel, television.Television.MIN_CHANNEL)
+
+    def test_power(self):
+        self.tv.power()
+        self.assertTrue(self.tv._Television__status)
+        self.tv.power()
+        self.assertFalse(self.tv._Television__status)
+
+    def test_mute(self):
+        # check if mute method doesn't mute tv is power is off
+        self.tv.mute()
+        self.assertFalse(self.tv._Television__muted)
+
+        # check if mute method works once the tv is on
+        self.tv.power()
+        self.tv.mute()
+        self.assertTrue(self.tv._Television__muted)
+        self.assertEqual(self.tv._Television__volume, television.Television.MIN_VOLUME)
+
+        # check if muting when already muted properly unmutes tv
+        self.tv.mute()
+        self.assertFalse(self.tv._Television__muted)
+
+        # check if muting/unmuting retains the original volume value
+        self.tv._Television__volume = 2
+        self.tv.mute()
+        self.assertTrue(self.tv._Television__muted)
+        self.tv.mute()
+        self.assertEqual(self.tv._Television__volume, 2)
+
+    def test_channel_up(self):
+        # checks that channel_up method does nothing when tv is off
+        self.tv.channel_up()
+        self.assertEqual(self.tv._Television__channel, television.Television.MIN_CHANNEL)
+
+        # checks if channel_up method properly increments the channel value when tv is on
+        self.tv.power()
+        self.tv.channel_up()
+        self.assertEqual(self.tv._Television__channel, television.Television.MIN_CHANNEL + 1)
+
+        # checks if channel_up method wraps channel value back around to the minimum value when used at the max channel
+        self.tv._Television__channel = television.Television.MAX_CHANNEL
+        self.tv.channel_up()
+        self.assertEqual(self.tv._Television__channel, television.Television.MIN_CHANNEL)
+
+    def test_channel_down(self):
+        # checks that channel_down method does nothing when tv is off
+        self.tv.channel_down()
+        self.assertEqual(self.tv._Television__channel, television.Television.MIN_CHANNEL)
+
+        # checks that channel_down method properly wraps the channel value around to the max value when used at the min
+        self.tv.power()
+        self.tv.channel_down()
+        self.assertEqual(self.tv._Television__channel, television.Television.MAX_CHANNEL)
+
+        # checks that the channel_down method properly decrements the channel value when the tv is on
+        self.tv.channel_down()
+        self.assertEqual(self.tv._Television__channel, television.Television.MAX_CHANNEL - 1)
+
+    def test_volume_up(self):
+        # checks that volume_up method does nothing when tv is off
+        self.tv.volume_up()
+        self.assertEqual(self.tv._Television__volume, television.Television.MIN_VOLUME)
+
+        # checks that volume_up method properly increments volume when tv is on
+        self.tv.power()
+        self.tv.volume_up()
+        self.assertEqual(self.tv._Television__volume, television.Television.MIN_VOLUME + 1)
+
+        # checks that the volume_up method does not increment the volume value if volume is already at max
+        self.tv._Television__volume = television.Television.MAX_VOLUME
+        self.tv.volume_up()
+        self.assertEqual(self.tv._Television__volume, television.Television.MAX_VOLUME)
+
+    def test_volume_down(self):
+        # checks that volume_down method does nothing when tv is off
+        self.tv.volume_down()
+        self.assertEqual(self.tv._Television__volume, television.Television.MIN_VOLUME)
+
+        # checks that volume_down method does nothing when tv is on and volume is already at minimum value
+        self.tv.power()
+        self.tv.volume_down()
+        self.assertEqual(self.tv._Television__volume, television.Television.MIN_VOLUME)
+
+        # checks that volume_down method properly decrements the volume value when tv is on
+        self.tv._Television__volume = television.Television.MAX_VOLUME
+        self.tv.volume_down()
+        self.assertEqual(self.tv._Television__volume, television.Television.MAX_VOLUME - 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
